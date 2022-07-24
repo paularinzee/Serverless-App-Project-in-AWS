cf88e318c2d7ccf1fb380af7baa2d640b250665c
Paul <arinzennajipual@gmail.com>
2022-07-24 09:42:41 -0700

    First commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '3vmkqn9vd6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-ygw21bb8.us.auth0.com',
+  clientId: 'bFn9fr4R0kYm66BseQvy6aYrAUivIWNF',
+  callbackUrl: 'http://localhost:3000/callback'
+};
